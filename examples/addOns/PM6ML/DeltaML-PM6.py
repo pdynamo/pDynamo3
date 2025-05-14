02b748464ad27659230185fe5fe227a2c17a7895
Guilherme M. Arantes <garantes@iq.usp.br>
2025-05-14 17:40:08 -0300

    Moved PM6-ML implementation to addOns

---
+++ examples/addOns/PM6ML/DeltaML-PM6.py
@@ -0,0 +1,35 @@
+"""Simple PM6-ML test."""
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# RESULTS (kJ/mol)
+# PM6: pDynamo -414.12  MOPAC/cuby -416.31
+# D3:  pDynamo  -80.73  MOPAC/cuby  -78.94
+# ML:  pDynamo -387.98  MOPAC/cuby -387.98
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import os.path
+
+from pCore              import Selection, TestScript_InputDataPath
+from pBabel             import ImportSystem 
+
+from pMolecule          import EnergyModelPriority
+from pMolecule.QCModel  import QCModelMNDO
+from pScientific        import Units
+
+from addOns.PM6ML       import QCDeltaMLModel
+
+# . The input data paths.
+dataPath = TestScript_InputDataPath ( "book" )
+molPath  = os.path.join ( dataPath, "mol" )
+
+# . Define the molecule.
+molecule  = ImportSystem ( os.path.join ( molPath, "bala_c7eq.mol" ) )
+
+# . Define the energy model with DeltaML correction.
+molecule.DefineQCModel  ( QCModelMNDO.WithOptions ( hamiltonian = "pm6" ) )
+eml = QCDeltaMLModel( ml_model_file = "PM6-ML_correction_seed8_best.ckpt")
+molecule.AddEnergyModel ('QC Delta-ML correction', eml, priority = EnergyModelPriority.QCAddOns )
+
+# . Calculate an energy.
+molecule.Energy( doGradients=True )
+
